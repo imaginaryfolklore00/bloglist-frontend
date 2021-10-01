b54216d209024b967b84c8a8e631d77264854832
Nazeem <nazeem.s2k@gmail.com>
2021-10-01 16:02:39 +0200

    5.17

---
+++ cypress/integration/blog_list.spec.js
@@ -0,0 +1,19 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Kate',
+      username: 'kt899',
+      password: 'fluid444'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Log in to application')
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
+  })
+})
